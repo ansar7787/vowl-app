1417e351221c4af89bb3ee0515a80e8070860d99
Ansar7787 <muhammedansar7787@gmail.com>
2026-08-02 12:39:00 +0530

    Optimize Intonation Mimic module

--- lib/features/accent/intonation_mimic/presentation/pages/intonation_mimic_screen.dart
+++ lib/features/accent/intonation_mimic/presentation/pages/intonation_mimic_screen.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:vowl/core/domain/entities/game_quest.dart';
 import 'package:vowl/core/presentation/themes/level_theme_helper.dart';
 import 'package:vowl/core/utils/haptic_service.dart';
@@ -259,7 +258,7 @@ class _IntonationMimicScreenState extends State<IntonationMimicScreen>
                                     SizedBox(height: gapTop),
                                     IntonationMimicInstruction(
                                       color: theme.primaryColor,
-                                      instruction: context.tr('games.intonation_mimic_instruction', fallback: quest.instruction),
+                                      instruction: context.tr('games.intonation_mimic_instruction', fallback: "Identify the intonation"),
                                     ),
                                     SizedBox(height: gapInstruction),
 
