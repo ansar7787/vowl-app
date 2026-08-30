046f2c075f0fefcb13593ec6d9de3c29dc1184e8
Ansar7787 <muhammedansar7787@gmail.com>
2026-08-30 09:42:08 +0530

    Fix(ContextClues): Remove unnecessary dart:ui import

--- lib/features/vocabulary/context_clues/presentation/widgets/context_clues_scanner.dart
+++ lib/features/vocabulary/context_clues/presentation/widgets/context_clues_scanner.dart
@@ -1,5 +1,5 @@
 import 'dart:math' as math;
-import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:vowl/features/vocabulary/context_clues/presentation/widgets/context_clues_painters.dart';
